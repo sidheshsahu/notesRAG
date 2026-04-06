75d8e48deaad8f35f08f29bbc28272e6475cce1c
sidheshsahu <sidhesh.sahu@somaiya.edu>
2026-04-06 18:15:38 +0530

    Added speech to text with multiple speakers

--- data/audio_text.py
+++ data/audio_text.py
@@ -1,22 +1,26 @@
-# Install the requests package by executing the command "pip install requests"
-
 import requests
 import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("ASSEMBLYAI_API_KEY")
 
 base_url = "https://api.assemblyai.com"
 
-headers = {"authorization": ""}
+headers = {"authorization": api_key}
 
 with open("./audio.mp3", "rb") as f:
     response = requests.post(base_url + "/v2/upload", headers=headers, data=f)
 
-audio_url = response.json()["upload_url"]
-
+upload_url = response.json()["upload_url"]
 
 data = {
-    "audio_url": audio_url,
-    "language_detection": True,
+    "audio_url": upload_url,
     "speech_models": ["universal-3-pro", "universal-2"],
+    "language_detection": True,
+    "speaker_labels": True,
 }
 
 url = base_url + "/v2/transcript"
@@ -27,10 +31,9 @@ polling_endpoint = base_url + "/v2/transcript/" + transcript_id
 
 while True:
     transcription_result = requests.get(polling_endpoint, headers=headers).json()
-    transcript_text = transcription_result["text"]
 
     if transcription_result["status"] == "completed":
-        print(f"Transcript Text:", transcript_text)
+        print(f"Transcript ID: {transcript_id}")
         break
 
     elif transcription_result["status"] == "error":
@@ -38,3 +41,6 @@ while True:
 
     else:
         time.sleep(3)
+
+for utterance in transcription_result["utterances"]:
+    print(f"Speaker {utterance['speaker']}: {utterance['text']}")
